d3cb019897b2da1a5a604469323913662299f69e
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-03 18:41:20 -0700

    Fixed bugs with alert

--- random.js
+++ random.js
@@ -2,9 +2,11 @@ function myRand() {
     var low = parseInt(document.getElementsByClassName('low')[0].value);
     var up = parseInt(document.getElementsByClassName('up')[0].value);
     if (up > 100){
+        alert ("Up cannot be greater than 100!");
         return;
     }
     if (low < 1 || low > up) {
+        alert ("low should be bigger than one and smaller then up!");
         return;
     }
     var result = Math.floor(Math.random() * (up - low + 1)) + low;
